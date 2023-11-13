0033d0e8b87123d72d256674e521cdb73b40b621
Tarik Horozovic <tarik.horoz@gmail.com>
2023-11-13 19:31:34 +0100

    vjezba cas 6

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,11 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 15;
+const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const userEnteredValue = parseInt(prompt("Enter the maximum life for you and the monster", "100"));
+
+const chosenMaxLife = processUserInput(userEnteredValue);
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -13,22 +16,31 @@ adjustHealthBars(chosenMaxLife);
 //Button listeners
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
 
 //Helper functions
+function processUserInput(value){
+  //u slucaju da je korinik unio nevalidnu vrijednost vratiti 100
+  //u suprotnom vratiti ono sto je korisnik unio
+}
+
 function attackMonster(mode) {
   let maxDamage;
   if (mode === "ATTACK") {
     maxDamage = ATTACK_VALUE;
   } else if (mode === "STRONG_ATTACK") {
     maxDamage = STRONG_ATTACK_VALUE;
-  }else{
-    console.log("Wrong attackMonster function parameters!")
+  } else {
+    console.log("Wrong attackMonster function parameters!");
     return;
   }
 
-  currentMonsterHealth -= dealMonsterDamage(attackDamage);
-  currentPlayerHealth -= dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentMonsterHealth -= dealMonsterDamage(maxDamage);
+  endRound();
+}
 
+function endRound() {
+  currentPlayerHealth -= dealPlayerDamage(MONSTER_ATTACK_VALUE);
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
@@ -36,6 +48,16 @@ function attackMonster(mode) {
   } else if (currentPlayerHealth < 0 && currentMonsterHealth <= 0) {
     alert("DRAW!");
   }
+
+  if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0){
+    reset();
+  }
+}
+
+function reset() {
+  resetGame(chosenMaxLife);
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
 }
 
 //Listener functions
@@ -43,7 +65,20 @@ function attackHandler() {
   attackMonster("ATTACK");
 }
 
-//Listener functions
 function strongAttackHandler() {
   attackMonster("STRONG_ATTACK");
+}
+
+function healPlayerHandler() {
+  let healValue;
+  // if(currentPlayerHealth >= chosenMaxLife - HEAL_VALUE)
+  if (currentPlayerHealth + HEAL_VALUE >= chosenMaxLife) {
+    healValue = chosenMaxLife - HEAL_VALUE;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
+  endRound();
 }
