c8e5e7444c0f8e59141f87c161fa97f88d4f48b4
Tarik Horozovic <tarik.horoz@gmail.com>
2023-11-13 18:31:22 +0100

    implementacija zadace

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 15;
+
 let chosenMaxLife = 100;
 
 let currentMonsterHealth = chosenMaxLife;
@@ -10,20 +12,38 @@ adjustHealthBars(chosenMaxLife);
 
 //Button listeners
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+
+//Helper functions
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }else{
+    console.log("Wrong attackMonster function parameters!")
+    return;
+  }
+
+  currentMonsterHealth -= dealMonsterDamage(attackDamage);
+  currentPlayerHealth -= dealPlayerDamage(MONSTER_ATTACK_VALUE);
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentPlayerHealth < 0 && currentMonsterHealth <= 0) {
+    alert("DRAW!");
+  }
+}
 
 //Listener functions
 function attackHandler() {
-  currentMonsterHealth -= dealMonsterDamage(ATTACK_VALUE);
-  currentPlayerHealth -= dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("You won!");
-  } 
-  else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("You lost!");
-  }
-  else if (currentPlayerHealth <0 && currentMonsterHealth <=0 )
-  {
-    alert("DRAW!");
-  }
+  attackMonster("ATTACK");
+}
+
+//Listener functions
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
 }
